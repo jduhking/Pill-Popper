8b0cb81066393643d5ba50fbcb2ceb8284c1de1b
jduhking <jamesbodebiyi@gmail.com>
2024-02-26 14:09:41 -0600

    Moved bluetooth to pairing

---
+++ mobile-app/models/dispense-service.tsx
@@ -0,0 +1,12 @@
+export interface DispenseObject {
+    slotNumber: number;
+    dispenseAmount: number;
+}
+
+const DISPENSE_SERVICE_UUID: string = '6fc3d9ab-3aef-4012-9456-15b0861e1139'
+const DISPENSE_CHARACTERISTIC_UUID: string = '10e6cc59-b033-48e8-bcf4-70390d05be0e'
+
+export {
+ DISPENSE_SERVICE_UUID,
+ DISPENSE_CHARACTERISTIC_UUID
+}
